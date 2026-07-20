e44aa5b93e82c1eb7a6337b982d1a915df579505
naiquire <naiquire2500@outlook.com>
2026-07-20 17:13:22 +0100

    installed ui framework

--- platformer-engine/platformer-engine/PlatformerEngine.cs
+++ platformer-engine/platformer-engine/PlatformerEngine.cs
@@ -1,3 +1,6 @@
+using Gum.Forms;
+using Gum.Forms.Controls;
+using MonoGameGum;
 using lib;
 using platformer_engine;
 
@@ -14,7 +17,35 @@ public class PlatformerEngine : Core
 
         ChangeScene(new LevelScene());
     }
-    
+
+    private void InitializeGum()
+    {
+        // Initialize the Gum service. The second parameter specifies
+        // the version of the default visuals to use. V3 is the latest
+        // version.
+        GumService.Default.Initialize(this, DefaultVisualsVersion.V3);
+
+        // Tell the Gum service which content manager to use. We will tell it to
+        // use the global content manager from our Core.
+        GumService.Default.ContentLoader.XnaContentManager = Core.Content;
+
+        // Register keyboard input for UI control.
+        FrameworkElement.KeyboardsForUiControl.Add(GumService.Default.Keyboard);
+
+        // Register gamepad input for Ui control.
+        FrameworkElement.GamePadsForUiControl.AddRange(GumService.Default.Gamepads);
+
+        // Customize the tab reverse UI navigation to also trigger when the keyboard
+        // Up arrow key is pushed.
+        FrameworkElement.TabReverseKeyCombos.Add(
+        new KeyCombo() { PushedKey = Microsoft.Xna.Framework.Input.Keys.Up });
+
+        // Customize the tab UI navigation to also trigger when the keyboard
+        // Down arrow key is pushed.
+        FrameworkElement.TabKeyCombos.Add(
+        new KeyCombo() { PushedKey = Microsoft.Xna.Framework.Input.Keys.Down });
+    }
+
     protected override void LoadContent()
     {
         base.LoadContent();
